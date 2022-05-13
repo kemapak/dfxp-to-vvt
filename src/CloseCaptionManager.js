d1495d8ebe588cf4efd59d34ede7653774976743
Kem Apak <kemalettin.apak@cvshealth.com>
2022-05-13 10:40:18 -0500

    Added parsing logic, code is running but in draft mode.

---
+++ src/CloseCaptionManager.js
@@ -0,0 +1,84 @@
+const readFile = require('./util.js');
+
+class CloseCaptionManager {
+
+    constructor() {
+    }
+
+    read(fileLocation) {
+        return readFile(fileLocation);
+    }
+
+    write() {
+
+    }
+
+    getBeginPropertyValue(text) {
+
+    }
+
+    getEndPropertyValue(text){
+
+    }
+
+    getTextValue(text) {
+
+    }
+
+    convert(inputText) {
+       // debugger;
+
+        // Remove formatting characters;
+        inputText = inputText.replace(/[\r\n]/g, '');
+
+       // debugger;
+
+        // Get the content in the body tag.
+        let bodyText = inputText.split('<body>')[1];
+        bodyText = bodyText.split('</body>')[0];
+
+        // Get the content in the div tag that have CC information.
+        let divText = bodyText.split('<div>')[1];
+        divText = divText.split('</div>')[0];
+
+        // Get each CC information within the P  tags.
+        let pCollection = divText.split('<p');
+
+        let ccCollection = [];
+        for (let index = 0, maxIndex = pCollection.length; index < maxIndex; index++) {
+            let ccFrame = {};
+            let frame = pCollection[index];
+            if (frame.indexOf('begin=') >= 0) {
+               // debugger;
+                let regionIndex = 0;
+                let values = frame.split('"');
+
+                ccFrame.begin = values[1];
+                ccFrame.end = values[3];
+                ccFrame.text= [];
+                ccFrame.text[regionIndex] = frame.match(/>(.*?)</)[1];
+
+                // The previous member could have the same begining and end so the text we got for the this current item will be the next line.
+                if (0 === ccCollection.length) {
+                    ccCollection.push(ccFrame);
+                    continue;
+                } else {
+                    let previousElement = ccCollection[ccCollection.length-1];
+                    if (previousElement.begin == ccFrame.begin){
+                        previousElement.text.push(ccFrame.text[regionIndex]);
+                        continue;
+                    } else {
+                        ccCollection.push(ccFrame);
+                    }
+                }
+
+             //   debugger;
+            }
+        }
+        let x = ccCollection;
+        debugger;
+
+    }
+}
+
+module.exports = CloseCaptionManager;
--- test/CloseCaptionManager.test.js
+++ test/CloseCaptionManager.test.js
@@ -1,12 +1,12 @@
-const Converter = require('../src/Converter');
 const readFile = require("../src/util");
+const CCConverter = require('../src/CloseCaptionManager');
 
 describe('Given a DFXP file ', () => {
 
     xtest('if a file does not exist should throw an exception', () => {
         let fileLocation = __dirname + '/asset/does-not-exists.txt';
 
-        let converter = new Converter();
+        let converter = new CCConverter();
 
         function wrapper() {
             converter.read(fileLocation);
@@ -17,18 +17,17 @@ describe('Given a DFXP file ', () => {
     xtest('if a file exist it should read the contents', () => {
         let fileLocation = __dirname + '/asset/test.txt';
 
-        let converter = new Converter();
+        let converter = new CCConverter();
 
         expect(converter.read(fileLocation)).toMatch(/This is a test text file./);
     });
 
-    test('should convert XML ot JSON', () => {
+    test('should convert XML to JSON', () => {
         let fileLocation = __dirname + '/asset/test-dfxp.xml';
 
-        let converter = new Converter();
+        let converter = new CCConverter();
         let xmlString = converter.read(fileLocation);
-        let json = converter.convert(xmlString);
 
-        // expect(converter.read(fileLocation)).toMatch(/This is a test text file./);
+        expect(converter.convert(xmlString)).toMatch(/This is a test text file./);
     });
 });
