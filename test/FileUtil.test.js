438f668b36f0f1c4eb4c3284b7b63d69bcad980a
Kem Apak <kemalettin.apak@cvshealth.com>
2022-05-16 01:27:29 -0500

    Added write method to the FileUtil, add the necessary test. Updated gitignore for the result files.

--- test/FileUtil.test.js
+++ test/FileUtil.test.js
@@ -1,6 +1,6 @@
 const FileUtil = require('../src/FileUtil.js');
 
-describe('Given a test file.', () => {
+describe('Given a test file, ', () => {
 
     test('if a file does not exist should throw an exception', () => {
         let fileLocation = __dirname + '/asset/does-not-exists.txt';
@@ -16,5 +16,18 @@ describe('Given a test file.', () => {
         let fileLocation = __dirname + '/asset/test.txt';
 
         expect(FileUtil.readFile(fileLocation)).toMatch(/This is a test text file./);
+    });
+});
+
+describe('Given sample text, ', () => {
+
+    test('should able to create a text file.', () => {
+
+        let fileLocation = __dirname + '/asset/result.txt';
+        let testString = 'This is text string.';
+
+        FileUtil.writeFile(fileLocation, testString);
+
+        expect(FileUtil.readFile(fileLocation)).toMatch(/This is text string./);
     });
 });
